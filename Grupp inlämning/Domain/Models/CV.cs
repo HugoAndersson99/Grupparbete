5515e0b386b076cb992dc49985f5c18c905b2268
antonjohanssoon <143598031+antonjohanssoon@users.noreply.github.com>
2025-01-07 14:50:32 +0100

    Cv,User and Contactdetails updated

--- Grupp inlämning/Domain/Models/CV.cs
+++ Grupp inlämning/Domain/Models/CV.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Models
+﻿namespace Domain.Models
 {
-    internal class CV
+    public class CV
     {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User User { get; set; }
+        public ContactDetails ContactDetails { get; set; }
+        public ICollection<WorkExperience> WorkExperiences { get; set; } = new List<WorkExperience>();
+        public ICollection<Education> Educations { get; set; } = new List<Education>();
+        public ICollection<Skill> Skills { get; set; } = new List<Skill>();
+        public About About { get; set; }
+
+        public CV() { }
+
+        public CV(string title, Guid userId)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            CreatedAt = DateTime.UtcNow;
+        }
     }
 }
--- Grupp inlämning/Domain/Models/User.cs
+++ Grupp inlämning/Domain/Models/User.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Models
+﻿namespace Domain.Models
 {
-    internal class User
+    public class User
     {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+
+        public ICollection<CV> CVs { get; set; } = new List<CV>();
+
+        public User() { }
+
+        public User(string email, string passwordHash, string role)
+        {
+            Id = Guid.NewGuid();
+            Email = email;
+            PasswordHash = passwordHash;
+            Role = role;
+        }
     }
 }
