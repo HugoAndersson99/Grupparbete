b21177094714c9ac2f43271837429bfd958aa921
cajsanilsson <143598251+cajsanilsson@users.noreply.github.com>
2025-01-07 14:27:37 +0100

    Added About, Education, Skill and Workexperience classes with properties.

---
+++ Grupp inlämning/Domain/Models/About.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class About
+    {
+        [Required]
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public int CvId { get; set; }
+        public CV Cv { get; set; }
+
+        public About(Guid id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+    }
+}
---
+++ Grupp inlämning/Domain/Models/WorkExperience.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class WorkExperience
+    {
+        [Required]
+        public Guid Id { get; set; }
+        public string CompanyName { get; set; }
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        public string City { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Guid CvId { get; set; }
+        public CV Cv { get; set; }
+
+        public WorkExperience(Guid id, string companyName, string jobTitle, string jobDescription, string city, DateTime startDate, DateTime endDate)
+        {
+            Id = id;
+            CompanyName = companyName;
+            JobTitle = jobTitle;
+            JobDescription = jobDescription;
+            City = city;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
