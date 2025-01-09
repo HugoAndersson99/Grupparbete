c87250359c6c2b51332aa889d6119bda614b4874
KattenBus <simon.oman.rinne@gmail.com>
2025-01-09 10:39:37 +0100

    Created some sample text for the Home page to test out Vercel.
    - Now you shouldn't get a 404 error when when running from developer mode.

--- Client/CV-Client/src/App.jsx
+++ Client/CV-Client/src/App.jsx
@@ -1,11 +1,13 @@
 import './Css/App.css'
 import Login_Page from './Pages/Login_Page.jsx';
+import Home_Page from './Pages/Home_Page.jsx';
 import { Routes, Route, Navigate } from 'react-router-dom';
 function App() {
   
 
   return (
     <Routes>
+      <Route path="/" element={<Home_Page />} />
       <Route path="/Login" element={<Login_Page />} />
     </Routes>
     
---
+++ Client/CV-Client/src/Pages/Home_Page.jsx
@@ -0,0 +1,11 @@
+import '../Css/Login_Page.css'
+
+function Home_Page() {
+  
+
+  return (
+    <div><h1>Hej! Detta är startsidan!</h1></div>
+  )
+};
+
+export default Home_Page;
