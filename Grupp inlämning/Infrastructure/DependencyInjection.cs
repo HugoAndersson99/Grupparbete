b8efc5c95d570594b876d19c77a6a17a924b8874
antonjohanssoon <143598031+antonjohanssoon@users.noreply.github.com>
2025-01-09 14:15:42 +0100

    Dependency Injections implemented

---
+++ Grupp inlämning/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces.RepositoryInterfaces;
+using Domain.Models;
+using Infrastructure.Databases;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionstring)
+        {
+            services.AddDbContext<Database>(options =>
+            {
+                options.UseSqlServer(connectionstring);
+            });
+
+            services.AddScoped<IRepository<About>, GenericRepository<About>>();
+            services.AddScoped<IRepository<ContactDetail>, GenericRepository<ContactDetail>>();
+            services.AddScoped<IRepository<CV>, GenericRepository<CV>>();
+            services.AddScoped<IRepository<Education>, GenericRepository<Education>>();
+            services.AddScoped<IRepository<Skill>, GenericRepository<Skill>>();
+            services.AddScoped<IRepository<User>, GenericRepository<User>>();
+            services.AddScoped<IRepository<WorkExperience>, GenericRepository<WorkExperience>>();
+
+            return services;
+        }
+    }
+}
