5e165109262a0f1cbfed0564e8de61ec6f82133c
RaashidM <raashidm24@gmail.com>
2025-01-12 14:56:18 +0100

    Created Login page and Register page

--- Client/CV-Client/src/App.jsx
+++ Client/CV-Client/src/App.jsx
@@ -2,6 +2,7 @@ import './Css/App.css'
 import Login_Page from './Pages/Login_Page.jsx';
 import Home_Page from './Pages/Home_Page.jsx';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Register_Page from './Pages/Register_Page.jsx';
 function App() {
   
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home_Page />} />
       <Route path="/Login" element={<Login_Page />} />
+      <Route path="/Register" element={<Register_Page />} />
     </Routes>
     
   )
