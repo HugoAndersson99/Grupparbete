38a4403c0d98d9d2db3756668e3a53a5012c4f89
Hugo.csharp@hotmail.com <Hugo.csharp@hotmail.com>
2025-01-15 10:55:46 +0100

    Klar med Cv-Commands! Lagt till Update och Delete samt ändrade namn på Add Mappen

---
+++ Grupp inlämning/Application/Commands/Cvs/UpdateCv/UpdateCvCommand.cs
@@ -0,0 +1,16 @@
+﻿using Application.Dtos;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Commands.Cvs.UpdateCv
+{
+    public class UpdateCvCommand : IRequest<OperationResult<CvDto>>
+    {
+        public UpdateCvCommand(CvDto cvToUpdate)
+        {
+            CvToUpdate = cvToUpdate;
+        }
+
+        public CvDto CvToUpdate { get; }
+    }
+}
