bc9f2f0fa1120990581441226377d36a402baa80
cajsanilsson <143598251+cajsanilsson@users.noreply.github.com>
2025-01-20 13:26:14 +0100

    added NUnit and integrationtests for queryhandlers

---
+++ Grupp inlämning/TestProject/UsersTests/LogInUserTests/LogInUserQueryHandlerTest.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces.RepositoryInterfaces;
+using Application.Queries.Users.Login.Helpers;
+using Application.Queries.Users.Login;
+using Domain.Models;
+using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Dtos;
+
+namespace TestProject.UsersTests.LogInUserTests
+{
+    
+}
+
