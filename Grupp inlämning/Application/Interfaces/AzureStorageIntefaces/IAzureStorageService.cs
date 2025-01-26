3dba077814cf52f04042d9a172d1cc8d7b5662df
Hugo.csharp@hotmail.com <Hugo.csharp@hotmail.com>
2025-01-26 15:14:18 +0100

    Tog Bort Connection string sålänge

---
+++ Grupp inlämning/Application/Interfaces/AzureStorageIntefaces/IAzureStorageService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Interfaces.AzureStorageIntefaces
+{
+    public interface IAzureStorageService
+    {
+        Task<string> UploadAsync(IFormFile file, string containerName);
+    }
+}
